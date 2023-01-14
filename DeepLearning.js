158b5fcae3ba8b747caf5be255875c7b88baee7c
master <onlybesthome@gmail.com>
2023-01-14 19:16:31 +0900

    step 3

--- DeepLearning.js
+++ DeepLearning.js
@@ -20,15 +20,26 @@
     function Square() {}
     Square.prototype.__proto__ = Function.prototype
     Square.prototype.forward = function(x) {
-        return x.map(v => Math.pow(v, 2))
+        return x.deepMap(v => Math.pow(v, 2))
+    }
+
+    function Exp() {}
+    Exp.prototype.__proto__ = Function.prototype
+    Exp.prototype.forward = function(x) {
+        return x.deepMap(v => Math.exp(v))
     }
 
 
-    let x = new Variable(Arr(10))
-    console.log(x.data)
-    let f = new Square()
-    let y = f.cal(x)
-    console.log(y.constructor)
+    let A = new Square()
+    let B = new Exp()
+    let C = new Square()
+
+    let x = new Variable(Arr(0.5))
+    let a = A.cal(x)
+    console.log(a)
+    let b = B.cal(a)
+    console.log(b)
+    let y = C.cal(b)
     console.log(y.data)
 
 })()
