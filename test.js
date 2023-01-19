8c93fec97262c2408be15a7094a0e1af19e803b4
master <onlybesthome@gmail.com>
2023-01-20 01:39:28 +0900

    step 27

--- test.js
+++ test.js
@@ -1,18 +1,59 @@
-const {Variable, no_grad} = require("./core")
+const {Variable, Operation} = require("./core")
 const Arr = require("./Arr")
 
-function sphere(x, y) {
-    let z = x.pow(2).add(y.pow(2))
-    return z
+function Sin() {
+    let result = Operation.call(this)
+    Object.setPrototypeOf(result, Sin.prototype)
+    return result
 }
 
-function matyas(x, y) {
-    let z = sphere(x, y).mul(0.26).sub(x.mul(y).mul(0.48))
-    return z
+Sin.prototype.__proto__ = Operation.prototype
+
+Sin.prototype.forward = function(x) {
+    let y = x.deepMap(v => Math.sin(v))
+    return y
 }
 
-let x = new Variable(Arr(1))
-let y = new Variable(Arr(1))
-let z = matyas(x, y)
-z.backward()
-console.log(x.grad, y.grad)
+Sin.prototype.backward = function(gy) {
+    let x = this.inputs[0].data
+    let gx = x.deepMap(v => Math.cos(v)).mul(gy)
+    return gx
+}
+
+function sin(x) {
+    return new Sin()(x)
+}
+
+let x = new Variable(Arr(Math.PI / 4))
+let y = sin(x)
+y.backward()
+console.log(y.data)
+console.log(x.grad)
+
+function factorial(a) {
+    let result = 1
+    for(let i = a; i > 1; i--) {
+        result *= i
+    }
+    return result
+}
+
+function my_sin(x, threshold) {
+    threshold = threshold === undefined ? 0.0001 : threshold
+    let y = 0
+    for(let i = 0; i < 100000; i++) {
+        let c = Math.pow(-1, i) / factorial(2 * i + 1)
+        let t = x.pow(2 * i + 1).mul(c)
+        y = t.add(y)
+        if(Math.abs(t.data) < threshold) {
+            break
+        }
+    }
+    return y
+}
+
+x = new Variable(Arr(Math.PI / 4))
+y = my_sin(x, 1e-10)
+y.backward()
+console.log(y.data)
+console.log(x.grad)
--- test.py
+++ test.py
@@ -277,100 +277,24 @@ def goldstein(x, y):
         (30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2))
     return z
 
-def _dot_var(v, verbose=False):
-    dot_var = '{} [label="{}", color=orange, style=filled]\n'
+class Sin(Function):
+    def forward(self, x):
+        y = np.sin(x)
+        return y
 
-    name = '' if v.name is None else v.name
-    if verbose and v.data is not None:
-        if v.name is not None:
-            name += ': '
-        name += str(v.shape) + ' ' + str(v.dtype)
-
-    return dot_var.format(id(v), name)
-
-
-def _dot_func(f):
-    # for function
-    dot_func = '{} [label="{}", color=lightblue, style=filled, shape=box]\n'
-    ret = dot_func.format(id(f), f.__class__.__name__)
-
-    # for edge
-    dot_edge = '{} -> {}\n'
-    for x in f.inputs:
-        ret += dot_edge.format(id(x), id(f))
-    for y in f.outputs:  # y is weakref
-        ret += dot_edge.format(id(f), id(y()))
-    return ret
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = gy * np.cos(x)
+        return gx
 
 
-def get_dot_graph(output, verbose=True):
-    """Generates a graphviz DOT text of a computational graph.
-    Build a graph of functions and variables backward-reachable from the
-    output. To visualize a graphviz DOT text, you need the dot binary from the
-    graphviz package (www.graphviz.org).
-    Args:
-        output (dezero.Variable): Output variable from which the graph is
-            constructed.
-        verbose (bool): If True the dot graph contains additional information
-            such as shapes and dtypes.
-    Returns:
-        str: A graphviz DOT text consisting of nodes and edges that are
-            backward-reachable from the output
-    """
-    txt = ''
-    funcs = []
-    seen_set = set()
-
-    def add_func(f):
-        if f not in seen_set:
-            funcs.append(f)
-            # funcs.sort(key=lambda x: x.generation)
-            seen_set.add(f)
-
-    add_func(output.creator)
-    txt += _dot_var(output, verbose)
-
-    while funcs:
-        func = funcs.pop()
-        txt += _dot_func(func)
-        for x in func.inputs:
-            txt += _dot_var(x, verbose)
-
-            if x.creator is not None:
-                add_func(x.creator)
-
-    return 'digraph g {\n' + txt + '}'
+def sin(x):
+    return Sin()(x)
 
 
-def plot_dot_graph(output, verbose=True, to_file='./graph.png'):
-    dot_graph = get_dot_graph(output, verbose)
-
-    tmp_dir = os.path.join(os.path.expanduser('~'), '.dezero')
-    if not os.path.exists(tmp_dir):
-        os.mkdir(tmp_dir)
-    graph_path = os.path.join(tmp_dir, 'tmp_graph.dot')
-    graph_path = "./tmp_graph.dot"
-
-    with open(graph_path, 'w') as f:
-        f.write(dot_graph)
-
-    extension = os.path.splitext(to_file)[1][1:]  # Extension(e.g. png, pdf)
-    cmd = 'dot {} -T {} -o {}'.format(graph_path, extension, to_file)
-    subprocess.run(cmd, shell=True)
-
-    # Return the image as a Jupyter Image object, to be displayed in-line.
-    
-    from IPython import display
-    return display.Image(filename=to_file)
-    
-
-
-x = Variable(np.array(1.0))
-y = Variable(np.array(1.0))
-z = goldstein(x, y)
-z.backward()
-
-x.name = 'x'
-y.name = 'y'
-z.name = 'z'
-print(plot_dot_graph(z, verbose=False))
+x = Variable(np.array(np.pi / 4))
+y = sin(x)
+y.backward()
+print('--- original sin ---')
+print(y.data)
+print(x.grad)
